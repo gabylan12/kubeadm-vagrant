b8aacc6d90a569ff0a3a5d673d53261d4ec86997
gabylan12 <gabriel.radikal@gmail.com>
2018-04-08 23:26:54 -0300

    add example with differents pods

---
+++ master/examples/lamp-differents-pods/php-code/index.php
@@ -0,0 +1,31 @@
+<?$enlace = mysqli_connect("lamp-service-differents-pods.default.svc.cluster.local",  "foo_user", "foo_password","foo_database");
+
+if (!$enlace) {
+    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Éxito: Se realizó una conexión apropiada a MySQL!!!! La base de datos mi_bd es genial." . PHP_EOL;
+echo "Información del host: " . mysqli_get_host_info($enlace) . PHP_EOL;
+echo "</br>";
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = mysqli_query($enlace, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($enlace);
+
+
+
+
+?>
