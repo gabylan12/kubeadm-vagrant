91e1f44796ef0bb97dc66d0593bb4529079ce16d
gabylan12 <gabriel.radikal@gmail.com>
2018-07-24 22:57:49 -0300

    working with kernel linux-image-4.9-amd64

---
+++ master-1/install/postinstall.bash
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+##########################################################################
+#
+# Install all requisites to execute kubernetes
+#
+##########################################################################
+
+#sudo su && echo 1 > /proc/sys/vm/drop_caches
+#INSTALL kubeadm https://kubernetes.io/docs/setup/independent/install-kubeadm/
+echo "Installing kubeadm ...."
+	apt-get update && apt-get install -y curl apt-transport-https 
+	curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+	echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list
+	apt-get update
+	apt-get install -y kubelet kubeadm kubectl
+
+	echo "cgroup-driver=cgroupfs" >> /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+	echo 'Environment="KUBELET_EXTRA_ARGS=--fail-swap-on=false"' >> /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+	echo 'export KUBECONFIG=/etc/kubernetes/admin.conf' >> /etc/profile
+	export KUBECONFIG=/etc/kubernetes/admin.conf
+echo "finish kubeadm installation."
+
+#Install dockerkub
+echo "installing docker ..."
+	apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+	add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
+	apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+echo "finish installing docker."
+
+#Reloading kubelet service
+echo "reloading kubelet..."
+    systemctl daemon-reload
+    systemctl restart kubelet
+echo "finish kubelet reload."
+
+#Install mysql server
+echo "install mysql ..."
+    sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
+    sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
+    sudo apt-get -y install mysql-server
+    mysql -h localhost -u  root --password=password < /examples/external-service/mysql-scripts/data.sql
+    sed -i -e 's/bind-address/#bind-address/g' /etc/mysql/my.cnf
+    service mysql restart
+echo "finish mysql."
+
---
+++ master-1/install/start.bash
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+IP=$1
+
+##########################################################################
+#
+# Start kubernetes service
+#
+##########################################################################
+
+#Reset kubeadm
+echo "reseting kubeadm..."
+    kubeadm reset
+    swapoff -a
+echo "kubeadm reset complete."
+
+#Starting kubeadm service
+echo "starting kubeadm ..."
+    kubeadm init  --apiserver-advertise-address=$IP --ignore-preflight-errors Swap #--fail-swap-on=false
+
+    #if you want to use environment variable without sudo user
+    mkdir -p $HOME/.kube
+    cp  /etc/kubernetes/admin.conf $HOME/.kube/config
+    chown vagrant:vagrant $HOME/.kube/config
+ #   export KUBECONFIG=$HOME/.kube/config
+
+    kubectl apply -f https://raw.githubusercontent.com/romana/romana/master/containerize/specs/romana-kubeadm.yml
+
+    #disable taints to execute pods inside master
+    kubectl patch node jessie-master -p '{"spec":{"taints":[]}}'
+echo "kubeadm started."
+
+echo "installing dashboard ..."
+  # follow https://dzone.com/articles/deploying-kubernetes-dashboard-to-a-kubeadm-create
+
+  kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+
+  #disable security dashboard
+  kubectl apply -f /install/setup-dashboard.yml
+
+
+echo 'proxy for dashboard'
+  sleep 30
+  echo "pulse skip button to see dashboard and follow this link"
+  echo "http://$IP:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/#!/login"
+echo "finish installing dashboard."
+
+  #start proxy
+  kubectl proxy --address=$IP --accept-hosts='^*$'
+echo "finish installing dashboard."
+
+
